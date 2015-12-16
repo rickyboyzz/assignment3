39fef66d8fc551da0b932f46a0ba28bdfbcd91d1
rickyboyzz <rkumar@mail.sfsu.edu>
2015-12-16 02:17:36 +0000

    Final commit

--- fb.js
+++ fb.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'appID' : '1665356590409458',
-	'appSecret' : 'f5f554727753d4ee49366919dad76699',
-	'callbackUrl' : 'https://assignment3-rickyboyzz.c9.io/login/facebook/callback'
+  'appID' : '1665356590409458',
+  'appSecret' : 'f5f554727753d4ee49366919dad76699',
+  'callbackUrl' : 'https://assignment3-rickyboyzz.c9users.io/login/facebook/callback'
 }
